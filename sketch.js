d28a5c960353a4b831cf03fae5ce2a1a931b46c1
alice <alicecomini7@gmail.com>
2025-10-18 10:50:13 +0200

    update sketch

--- sketch.js
+++ sketch.js
@@ -2,46 +2,45 @@ let table;
 
 function preload() {
   // put preload code here
-  table = loadTable("assets/dataset.csv","csv","header");
+  table = loadTable("assets/dataset.csv", "csv", "header");
 }
 
 function setup() {
-  //controllo se ho caricato i dati
+  // controllo se ho caricato i dati
 
-  let outerPadding = 20; 
-  let padding = 10; 
-  let itemSize = 30; 
+  let outerPadding = 20;
+  let padding = 10;
+  let itemSize = 30;
 
-  //calcolo il numero delle colonne
-  let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding)); // arrotondo per difetto --> meglio una colonna in meno che una che sborda
+  // calcolo il numero di colonne
+  let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding));
 
-  let rows = ceil(table.getRowCount() / cols); //numero di righe / il numero di colonne
+  let rows = ceil(table.getRowCount() / cols);
 
   let totalHeight = outerPadding * 2 + rows * itemSize + (rows - 1) * padding;
 
   //creo il canvas
-  createCanvas(windowWidth, totalHeight); //larghezza finestra, altezza calcolata
+  createCanvas(windowWidth, totalHeight);
 
   background("coral");
 
-  console.log("cols: ", cols, "rows: ", rows);
+  console.log("cols: ", cols, " rows: ", rows);
 
   let colCount = 0;
   let rowCount = 0;
 
-  //esegue il ciclo per ogni riga
-  for(let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++){
-    //carico i dati dalla tabella
+  for (let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++) {
+    //carico dati della riga
     let data = table.getRow(rowNumber).obj;
-    
-    //prendo valore per dimensione
-    let myValue = data['column0'];
 
-    //calcolo min e max della colonna
+    // prendo valore per dimensione
+    let myValue = data["column0"];
+
+    //calcolo min e massimo
     let allValues = table.getColumn("column0");
     let minValue = min(allValues);
     let maxValue = max(allValues);
-    let scaledValue = map(myValue, minValue, maxValue, 1, itemSize); //valore che vogliamo scalare, valore minimo della scala di partenza, valore max, dim. + piccola, dim. + grande 
+    let scaledValue = map(myValue, minValue, maxValue, 1, itemSize);
 
     //seconda variabile per il colore
     let value2 = data["column2"];
@@ -50,32 +49,30 @@ function setup() {
     let maxValue2 = max(allValues2);
     let value2Mapped = map(value2, minValue2, maxValue2, 0, 1);
 
-    let c1 = color("lightblue");
+    let c1 = color("yellow");
     let c2 = color("blue");
 
     let mappedColor = lerpColor(c1, c2, value2Mapped);
 
-    fill(mappedcolor)
+    fill(mappedColor);
 
-    //posizione x e y
     let xPos = outerPadding + colCount * (itemSize + padding);
+
     let yPos = outerPadding + rowCount * (itemSize + padding);
 
-    //rettangolo per ogni valore del dataset
-    rect(xPos, yPos, scaledValue, scaledValue) // x, y, largh., altez.
+    rect(xPos, yPos, scaledValue, scaledValue);
 
-    //ad ogni ciclo aumento numero colonne
+    // aumento colcount
     colCount++;
 
-    //controllo se siamo a fine riga
-    if(colCount == cols){ //se è uguale al numero di colonne
-      colCount = 0; //conteggio colonne torna 0
-      rowCount++; //conteggio righe aumenta di uno
+    // controllo se siamo a fine riga
+    if (colCount == cols) {
+      colCount = 0;
+      rowCount++;
     }
   }
 }
 
-
 function draw() {
-  // put drawing code here
+  // put drawing code here
 }
